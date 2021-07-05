8f9b6f171e2859c1d72122d05db6cc4c4a75791d
Gugulothuhimabindu <69143849+Gugulothuhimabindu@users.noreply.github.com>
2021-07-05 22:04:30 +0530

    Create  CharToStringAndStringToChar.java

---
+++ JP LAB/Cycle 1/CharToStringAndStringToChar.java
@@ -0,0 +1,13 @@
+public class CharToStringAndStringToChar
+{
+   public static void main(String args[])
+   {
+      char ch = 'a';
+      String str = "Hello";
+      for(int i=0; i<str.length();i++){
+      	ch = str.charAt(i);
+        System.out.println("Character at "+i+" Position: "+ch);
+      }
+      System.out.println("String is: "+Character.toString(ch));
+   }
+}
