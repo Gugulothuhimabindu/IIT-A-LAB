d383949758a897ff055e808b1d5fb8911bf09e3d
Gugulothuhimabindu <69143849+Gugulothuhimabindu@users.noreply.github.com>
2021-07-05 22:48:34 +0530

    Update MultiplyTwoNumbers.java

--- JP LAB/Cycle 1/MultiplyTwoNumbers.java
+++ JP LAB/Cycle 1/MultiplyTwoNumbers.java
@@ -1,6 +1,6 @@
 public class MultiplyTwoNumbers{
 public static void main(String args[]){
-int num1 = 34,num2 = 45,product;
+int num1 = 55,num2 = 65,product;
 product=num1*num2;
 System.out.println("The product of given two numbers is: "+product);
 }
