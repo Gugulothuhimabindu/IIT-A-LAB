fbea02e39a696c87ba041052c6230d791be10f07
Gugulothuhimabindu <69143849+Gugulothuhimabindu@users.noreply.github.com>
2021-08-01 21:39:47 +0530

    Update CharToStringAndStringToChar.java

--- JP LAB/Cycle 1/CharToStringAndStringToChar.java
+++ JP LAB/Cycle 1/CharToStringAndStringToChar.java
@@ -3,7 +3,7 @@ public class CharToStringAndStringToChar
    public static void main(String args[])
    {
       char ch = 'a';
-      String str = "Hello";
+      String str = "BINDU";
       for(int i=0; i<str.length();i++){
       	ch = str.charAt(i);
         System.out.println("Character at "+i+" Position: "+ch);
